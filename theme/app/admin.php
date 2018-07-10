d848c5de88c2373e3cd1d7fd6e26051c2192a1be
Igor Spasić <igor.spasic@gmail.com>
2018-07-10 17:54:28 +0200

    source formatting

--- theme/app/admin.php
+++ theme/app/admin.php
@@ -144,6 +144,10 @@ add_action('customize_register', function (\WP_Customize_Manager $wp_customize) 
 /**
  * Customizer JS
  */
-add_action('customize_preview_init', function () {
+add_action('customize_preview_init', function() {
     wp_enqueue_script('sage/customizer.js', asset_path('scripts/customizer.js'), ['customize-preview'], null, true);
 });
+
+/**
+ * Admin styles.
+ */
